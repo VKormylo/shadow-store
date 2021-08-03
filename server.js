45dad84d7e96d4c6a166e787550943f5a8cb69da
VKormylo <vitaliykormylo@gmail.com>
2021-08-03 17:15:33 +0300

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const PORT = 8060;
+const PORT = process.env.PORT || 8060;
 
 app.use(express.json());
 app.use(bodyParser.json());
