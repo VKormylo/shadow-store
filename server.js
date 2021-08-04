8336a4ddf640ace26675dbf390c7b02255966e27
VKormylo <vitaliykormylo@gmail.com>
2021-08-04 14:30:45 +0300

    update

--- server.js
+++ server.js
@@ -784,8 +784,10 @@ app.post("/language", (req, res) => {
   });
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
