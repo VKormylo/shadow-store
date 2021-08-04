38dee1bec7f4a0bf3911f045877f0149406218fc
VKormylo <vitaliykormylo@gmail.com>
2021-08-04 12:34:29 +0300

    update

--- server.js
+++ server.js
@@ -787,11 +787,10 @@ app.use(express.static(path.join(__dirname, "client/build")));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
-    res.sendfile(path.join((__dirname = "client/build/index.html")));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/public/index.html"));
-});
+
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
