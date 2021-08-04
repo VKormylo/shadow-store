9d335eaa8ffed48672b3661c753496b96aec1281
VKormylo <vitaliykormylo@gmail.com>
2021-08-04 13:13:42 +0300

    update

--- server.js
+++ server.js
@@ -784,11 +784,12 @@ app.post("/language", (req, res) => {
   });
 });
 
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-
+}
 
 
 
