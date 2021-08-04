2ec7f14383ed520149e5a8132efb6a075dabb928
VKormylo <vitaliykormylo@gmail.com>
2021-08-04 20:25:33 +0300

    update

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,60 +1,54 @@
 import * as axios from "axios";
 
-const instance = axios.create({
-  withCredentials: false,
-  dataType: "jsonp",
-  baseURL: "https://shadow-store.herokuapp.com",
-});
-
 export const shopAPI = {
   getStore() {
-    return instance.get("/games").then((response) => {
+    return axios.get("/games").then((response) => {
       return response.data;
     });
   },
   getStatus() {
-    return instance.get("/store/status");
+    return axios.get("/store/status");
   },
   changeStatus(status) {
-    return instance.post("/store/status", {status});
+    return axios.post("/store/status", {status});
   },
 };
 
 export const libraryAPI = {
   addToLibrary(gameId) {
-    return instance.post("/game/addtolibrary", {gameId});
+    return axios.post("/game/addtolibrary", {gameId});
   },
   removeFromLibrary(gameId) {
-    return instance.post("/game/removefromlibrary", {gameId});
+    return axios.post("/game/removefromlibrary", {gameId});
   },
   downloadGame(gameId) {
-    return instance.post("/game/download", {gameId});
+    return axios.post("/game/download", {gameId});
   },
   deleteGame(gameId) {
-    return instance.post("/game/delete", {gameId});
+    return axios.post("/game/delete", {gameId});
   },
   getView() {
-    return instance.get("/library/view");
+    return axios.get("/library/view");
   },
   changeView(text) {
-    return instance.put("/library/view", {text});
+    return axios.put("/library/view", {text});
   },
 };
 
 export const wishlistAPI = {
   addToWishlist(gameId) {
-    return instance.post("/game/addtowishlist", {gameId});
+    return axios.post("/game/addtowishlist", {gameId});
   },
   removeFromWishlist(gameId) {
-    return instance.post("/game/removefromwishlist", {gameId});
+    return axios.post("/game/removefromwishlist", {gameId});
   },
 };
 
 export const languageAPI = {
   getLanguage() {
-    return instance.get("/language");
+    return axios.get("/language");
   },
   changeLanguage(language) {
-    return instance.post("/language", {language});
+    return axios.post("/language", {language});
   },
 };
